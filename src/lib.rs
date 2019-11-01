ede0ef48caa8006894002a8978391f8d73be8489
Sander in 't Hout <sander@inthout.eu>
2019-11-01 12:19:12 +0100

    add untested function for creating new WIM

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 pub mod wimimage;
+use wimlib_sys::*;
+use std::ffi::CStr;
+
+pub enum WimlibError {
+    GeneralLibraryError
+}
+
+pub fn get_wimlib_version() -> Result<&'static str, WimlibError> {
+    unsafe {
+        let c_buf = wimlib_get_version_string();
+        let c_str: &CStr = CStr::from_ptr(c_buf);
+        c_str.to_str().map_err(|_| WimlibError::GeneralLibraryError)
+    }
+}
--- src/wimimage.rs
+++ src/wimimage.rs
@@ -7,13 +7,36 @@ pub struct WimImage {
     c_struct: *mut wimlib_sys::WIMStruct
 }
 
-pub enum WimImageOpenError {
-    InvalidCharacters,
+pub enum WimCreateError {
     UnknownWimlibError(i32)
 }
 
+pub enum WimImageOpenError {
+    UnknownWimlibError(i32),
+    InvalidCharacters
+}
+
 impl WimImage {
-    fn open_from_file(wim_file_path: &Path) -> Result<Self, WimImageOpenError> {
+    pub fn new() -> Result<Self, WimCreateError> {
+        let wim_struct_ptr_box = Box::new(ptr::null_mut());
+        let wim_struct_ptr_box_ptr = Box::into_raw(wim_struct_ptr_box);
+        unsafe {
+            let create_res = wimlib_create_new_wim(wimlib_compression_type::WIMLIB_COMPRESSION_TYPE_NONE, wim_struct_ptr_box_ptr);
+            match (create_res) {
+                0 => {
+                    // success
+                    let wim_struct_ptr_box = Box::from_raw(wim_struct_ptr_box_ptr);
+                    let wim_struct_ptr = *wim_struct_ptr_box;
+                    Ok(WimImage { c_struct: wim_struct_ptr })
+                },
+                _ => {
+                    Err(WimCreateError::UnknownWimlibError(create_res))
+                }
+            }
+        }
+    }
+
+    pub fn open_from_file(wim_file_path: &Path) -> Result<Self, WimImageOpenError> {
         let path = wim_file_path.to_str().ok_or_else(|| WimImageOpenError::InvalidCharacters)?;
         let cpath = CString::new(path).map_err(|_| WimImageOpenError::InvalidCharacters)?;
         let wim_struct_ptr_box = Box::new(ptr::null_mut());
